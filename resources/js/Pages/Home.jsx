1253965ad9a0ef6a0f341e118dd69e3b31023189
jaoharil <jaoharilmaknun2904@gmail.com>
2024-01-09 15:57:40 +0700

    intsal packed

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <React.Fragment>
+            <h1>welcome</h1>
+        </React.Fragment>
+    );
+};
+
+export default Home;
---
+++ resources/js/app.js
@@ -0,0 +1,31 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes React and other helpers. It's a great starting point while
+ * building robust, powerful web applications using React + Laravel.
+ */
+
+// import './bootstrap';
+
+/**
+ * Next, we will create a fresh React component instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+// import './components/Example';
+import { createInertiaApp } from "@inertiajs/react";
+import { createRoot } from "react-dom/client";
+
+createInertiaApp({
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/Home.jsx", { eager: true });
+        return pages[`./Pages/${name}.jsx`];
+    },
+    setup({ el, App, props }) {
+        createRoot(el).render(<App {...props} />);
+    },
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/Home.jsx", { eager: true });
+        return pages[`./Pages/${name}.jsx`];
+    },
+});
